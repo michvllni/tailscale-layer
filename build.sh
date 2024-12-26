b1d026ca39e8f099da0a7cacc65ce413b53105b3
Michael Villani <45660891+michvllni@users.noreply.github.com>
2024-12-26 18:00:56 +0000

    feat: add pysocks to the layer and accept tailscale routes

--- build.sh
+++ build.sh
@@ -8,12 +8,16 @@ yum -y install tailscale
 
 mkdir /tmp/extensions
 mkdir /tmp/bin
+mkdir -p /tmp/python/lib/python3.9/site-packages
+
 cd /tmp/extensions
 cp /opt/layer/extension1.sh .
 cp /opt/layer/curl /tmp/bin/
 cp /usr/bin/tailscale /tmp/bin/
 cp /usr/sbin/tailscaled /tmp/bin
+pip install pysocks -t /tmp/python/lib/python3.9/site-packages/
 
 cd /tmp
 zip -r /opt/layer/tailscale.zip extensions 
 zip -r /opt/layer/tailscale.zip bin
+zip -r /opt/layer/tailscale.zip python 
--- connect.sh
+++ connect.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-until tailscale up --authkey=$KEY
+until tailscale up --authkey=$KEY --accept-routes
 do
     sleep 0.1
 done
